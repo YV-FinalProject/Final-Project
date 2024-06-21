90d25c27bb9452a4243cfdf3b9abc609464f2994
4rad95 <4rad@i.ua>
2024-06-21 21:21:09 +0200

    SCRUM-17, SCRUM-18

--- src/main/resources/db/changelog/changeSets.v0.1/createTables.sql
+++ src/main/resources/db/changelog/changeSets.v0.1/createTables.sql
@@ -4,21 +4,21 @@
 CREATE TABLE Users (UserID BIGINT AUTO_INCREMENT NOT NULL, Name VARCHAR(255) NULL, Email VARCHAR(255) NULL, PhoneNumber VARCHAR(255) NULL, PasswordHash VARCHAR(255) NULL, Role ENUM('CLIENT', 'ADMINISTRATOR') NULL, CONSTRAINT PK_USERS PRIMARY KEY (UserID));
 
 -- changeset yulia:create_table_products
-CREATE TABLE Products (ProductID BIGINT AUTO_INCREMENT NOT NULL, CategoryID BIGINT NOT NULL, Price DECIMAL (10, 2) NULL, DiscountPrice DECIMAL (10, 2) NULL, CreatedAt datetime NULL, UpdatedAt datetime NULL, Name VARCHAR(255) NULL, Description VARCHAR(255) NULL, ImageURL VARCHAR(255) NULL, CONSTRAINT PK_PRODUCTS PRIMARY KEY (ProductID));
+CREATE TABLE Products (ProductID BIGINT AUTO_INCREMENT NOT NULL, CategoryID BIGINT NULL, Price DECIMAL (10, 2) NULL, DiscountPrice DECIMAL (10, 2) NULL, CreatedAt datetime NULL, UpdatedAt datetime NULL, Name VARCHAR(255) NULL, Description VARCHAR(255) NULL, ImageURL VARCHAR(255) NULL, CONSTRAINT PK_PRODUCTS PRIMARY KEY (ProductID));
 
 -- changeset yulia:create_table_categories
 CREATE TABLE Categories (CategoryID BIGINT AUTO_INCREMENT NOT NULL, Name VARCHAR(255) NULL, CONSTRAINT PK_CATEGORIES PRIMARY KEY (CategoryID));
 
 -- changeset yulia:create_table_cart
 CREATE TABLE Cart (CartID BIGINT AUTO_INCREMENT NOT NULL,
-UserID BIGINT NOT NULL, CONSTRAINT PK_CART PRIMARY KEY (CartID), UNIQUE (UserID));
+UserID BIGINT NULL, CONSTRAINT PK_CART PRIMARY KEY (CartID), UNIQUE (UserID));
 
 -- changeset yulia:create_table_cartitems
-CREATE TABLE CartItems (CartItemID BIGINT AUTO_INCREMENT NOT NULL, CartID BIGINT NOT NULL,
-ProductID BIGINT NOT NULL, Quantity INT NULL, CONSTRAINT PK_CARTITEMS PRIMARY KEY (CartItemID));
+CREATE TABLE CartItems (CartItemID BIGINT AUTO_INCREMENT NOT NULL, CartID BIGINT  NULL,
+ProductID BIGINT  NULL, Quantity INT NULL, CONSTRAINT PK_CARTITEMS PRIMARY KEY (CartItemID));
 
 -- changeset yulia:create_table_favorites
-CREATE TABLE Favorites (FavoriteID BIGINT AUTO_INCREMENT NOT NULL, ProductID BIGINT NOT NULL, UserID BIGINT NOT NULL, CONSTRAINT PK_FAVORITES PRIMARY KEY (FavoriteID));
+CREATE TABLE Favorites (FavoriteID BIGINT AUTO_INCREMENT NOT NULL, ProductID BIGINT  NULL, UserID BIGINT  NULL, CONSTRAINT PK_FAVORITES PRIMARY KEY (FavoriteID));
 
 -- changeset yulia:create_table_orders
 CREATE TABLE Orders (OrderID BIGINT AUTO_INCREMENT NOT NULL, UserID BIGINT NULL, CreatedAt datetime NULL, DeliveryAddress VARCHAR(255) NULL, ContactPhone VARCHAR(255) NULL, DeliveryMethod VARCHAR(255) NULL, Status ENUM('CREATED','PENDING PAYMENT', 'PAID', 'ON THE WAY', 'DELIVERED', 'CANCELED') NULL, UpdatedAt datetime NULL, CONSTRAINT PK_ORDERS PRIMARY KEY (OrderID));
@@ -35,7 +35,7 @@ ALTER TABLE Products ADD CONSTRAINT foreign_key_products_categories FOREIGN KEY 
 ALTER TABLE Favorites ADD CONSTRAINT foreign_key_favorites_users FOREIGN KEY (UserID) REFERENCES Users (UserID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- changeset yulia:create_foreign_key_favorites_products
--- ALTER TABLE Favorites ADD CONSTRAINT foreign_key_favorites_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE Favorites ADD CONSTRAINT foreign_key_favorites_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- changeset yulia:create_foreign_key_cart_users
 ALTER TABLE Cart ADD CONSTRAINT foreign_key_cart_users FOREIGN KEY (UserID) REFERENCES Users (UserID) ON UPDATE RESTRICT ON DELETE RESTRICT;
@@ -44,7 +44,7 @@ ALTER TABLE Cart ADD CONSTRAINT foreign_key_cart_users FOREIGN KEY (UserID) REFE
 ALTER TABLE CartItems ADD CONSTRAINT foreign_key_cartitems_cart FOREIGN KEY (CartID) REFERENCES Cart (CartID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- changeset yulia:create_foreign_key_cartitems_products
--- ALTER TABLE CartItems ADD CONSTRAINT foreign_key_cartitems_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE CartItems ADD CONSTRAINT foreign_key_cartitems_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- changeset yulia:create_foreign_key_orders_users
 ALTER TABLE Orders ADD CONSTRAINT foreign_key_orders_users FOREIGN KEY (UserID) REFERENCES Users (UserID) ON UPDATE RESTRICT ON DELETE RESTRICT;
@@ -53,7 +53,7 @@ ALTER TABLE Orders ADD CONSTRAINT foreign_key_orders_users FOREIGN KEY (UserID) 
 ALTER TABLE OrderItems ADD CONSTRAINT foreign_key_orderitems_orders FOREIGN KEY (OrderID) REFERENCES Orders (OrderID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- changeset yulia:create_foreign_key_orderitems_products
--- ALTER TABLE OrderItems ADD CONSTRAINT foreign_key_orderitems_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
+ALTER TABLE OrderItems ADD CONSTRAINT foreign_key_orderitems_products FOREIGN KEY (ProductID) REFERENCES Products (ProductID) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 
 
