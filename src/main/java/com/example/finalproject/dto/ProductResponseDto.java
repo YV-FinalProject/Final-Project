8c4933cd62f49047ccaa009c2786d7811e0144b8
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-06-23 20:01:42 +0200

    fixed-product-dtos

--- src/main/java/com/example/finalproject/dto/ProductResponseDto.java
+++ src/main/java/com/example/finalproject/dto/ProductResponseDto.java
@@ -23,44 +23,16 @@ import org.hibernate.annotations.CreationTimestamp;
 @Builder
 public class ProductResponseDto {
 
-    @Id
-    @Column(name = "ProductID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
-
-    @NotBlank(message = "Invalid name: Empty name")
     private String name;
-
-    @NotBlank(message = "Invalid description: Empty description")
     private String description;
-
-    @DecimalMin(value = "0.0")
-    @Digits(integer=4, fraction=2)
     private BigDecimal price;
-
-    @DecimalMin(value = "0.0")
-    @Digits(integer=4, fraction=2)
     private BigDecimal discountPrice;
-
-    @CreationTimestamp
-    @Column(name = "CreatedAt")
     private Timestamp createdAt;
-
-    @CreationTimestamp
-    @Column(name = "UpdatedAt")
     private Timestamp updatedAt;
-
-   @NotBlank(message = "Invalid image: Empty imageURL")
-    // @Pattern(regexp = "^(?:[\w]\:|\\)(\\[a-z_\-\s0-9\.]+)+\.(txt|gif|pdf|doc|docx|xls|xlsx)$", message = "Invalid URL")
     private String imageURL;
-
     private Long CategoryId;
 
-
-//    @JsonInclude(JsonInclude.Include.NON_NULL)
-//    @JsonProperty("category")
-//    @NotBlank(message = "Invalid Category: Empty Category")
-//    private String category;
 
 //    @JsonInclude(JsonInclude.Include.NON_NULL)
 //    @JsonProperty("category")
