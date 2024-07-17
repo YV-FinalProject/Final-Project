c05c31dfbd69851663bf34619569adc3180038bd
4rad95 <4rad@i.ua>
2024-07-17 14:58:25 +0200

    Log with record

--- src/main/java/com/example/finalproject/dto/querydto/ProductCountDto.java
+++ src/main/java/com/example/finalproject/dto/querydto/ProductCountDto.java
@@ -1,9 +1,6 @@
 package com.example.finalproject.dto.querydto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.id.IntegralDataTypeHolder;
 
 import java.math.BigDecimal;
@@ -17,4 +14,5 @@ public class ProductCountDto {
     private String status;
     private Integer count;
     private BigDecimal sum;
+
 }
